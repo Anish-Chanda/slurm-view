e2c539f9f86525dbee798b86b2a8898aa3282df7
Anish Chanda <anish.pvt03@gmail.com>
2025-03-26 10:22:38 -0500

    migrate to handlebars

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import { getSlurmJobs } from './handlers/fetchJobs.js';
+import { engine } from 'express-handlebars';
+
 
 const app = express();
 const port = 3000;
+
+//handlebars config
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
 const router = express.Router();
 app.use(process.env.PASSENGER_BASE_URI || '/', router);
@@ -14,24 +21,10 @@ router.get('/api/jobs', async (req, res) => {
 
 router.get('/', async (req, res) => {
   const jobsTable = await getSlurmJobs();
-  res.send(`
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <meta charset="utf-8" />
-      <title>Slurm Jobs</title>
-      <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    </head>
-    <body class="font-sans m-5 p-5 bg-gray-100">
-      <h1 class="text-center text-gray-800 mb-6 text-2xl">Slurm Job Queue</h1>
-      
-      <!-- Jobs Table Container -->
-      <div id="jobs-table">
-        ${jobsTable}
-      </div>
-    </body>
-    </html>
-  `);
+  res.render('home', {
+    title: "Slurm View",
+    jobsTable: jobsTable
+  })
 });
 
 app.listen(port, () => {
