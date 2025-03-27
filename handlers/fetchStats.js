f26b6b4e2e824f376dfad3f8a8b8b8eab1da6b3a
Anish Chanda <anish.pvt03@gmail.com>
2025-03-27 13:21:44 -0500

    show a sunburst chart for CPUs by state

---
+++ handlers/fetchStats.js
@@ -0,0 +1,13 @@
+import { executeCommand } from "../helpers/executeCmd.js";
+
+// Fetches the number of CPUs by state
+export function getCPUsByState() {
+    try {
+        const cmdOutput = executeCommand("sinfo -o '%C' --noheader") // returns cpu utilization in Allocated/Idle/Other/Total
+        const [allocated, idle, other, total] = cmdOutput.split('/').map(Number);
+        return { allocated, idle, other, total };
+    } catch (error) {
+        console.error('Error in getCPUsByState:', err.message);
+        return `<p>Error retrieving utilization statistics: ${err.message}</p>`;
+    }
+}
