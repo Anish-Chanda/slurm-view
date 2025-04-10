4769740858ec1ca57e537389e17e5d47f7ba7688
Anish Chanda <anish.pvt03@gmail.com>
2025-04-10 14:29:59 -0500

    bump exec command's buffer to 20MB

--- helpers/executeCmd.js
+++ helpers/executeCmd.js
@@ -3,7 +3,7 @@ const { execSync } = require('child_process');
 // This function is used to execute shell commands and return the output.
 function executeCommand(command) {
     try {
-        const output = execSync(command, { maxBuffer: 1024 * 1024 * 15 });
+        const output = execSync(command, { maxBuffer: 1024 * 1024 * 20 });
         return output.toString().trim();
     } catch (e) {
         throw new Error("Failed to execute command: " + command + " message: " + e.message);
